1eb351a38e27d64a1060deb14cfe5d53d699feaf
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2017-07-27 01:19:20 -0300

    adicionando middleware compression

--- backend/src/libs/middlewares.js
+++ backend/src/libs/middlewares.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from 'body-parser';
+import compression from 'compression';
 import cors from 'cors';
 
 module.exports = (app) => {
@@ -7,10 +8,9 @@ module.exports = (app) => {
 
     app.set('port', config.server_port);
     app.set('json spaces', 4);
-
+    
+    app.use(compression());
     app.use(cors());
-
     app.use(bodyParser.json());
-
     app.use(express.static('public'));
 }
