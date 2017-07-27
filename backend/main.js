2d0d2f01ca3b1b2edfb536f778bf0a7ce9990b0a
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2017-07-27 00:17:43 -0300

    exemplo do operador spread do es2015 utilizado nas funcoes do DB

---
+++ backend/main.js
@@ -0,0 +1,14 @@
+
+function exemplo({nrAno, nrMes, nrDia, nomeMae}){
+    console.log(nrAno,nrMes,nrDia);
+    console.log(nomeMae);
+}
+
+var pessoa = {
+    nome : "Giliard",
+    nrAno : 2015,
+    nrMes : 4,
+    nrDia : 20
+}
+
+exemplo(pessoa);
