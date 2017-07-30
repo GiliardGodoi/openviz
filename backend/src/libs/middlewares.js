89708eefafc5b78adb03b72bb6ab8458cc96f926
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2017-07-30 20:32:53 -0300

    habilitando o compression

--- backend/src/libs/middlewares.js
+++ backend/src/libs/middlewares.js
@@ -1,5 +1,5 @@
 import express from "express";
-import bodyParser from 'body-parser';
+import bodyparser from 'body-parser';
 import compression from 'compression';
 import cors from 'cors';
 
@@ -8,9 +8,10 @@ module.exports = (app) => {
 
     app.set('port', config.server_port);
     app.set('json spaces', 4);
+    app.use(bodyparser.json());
+    app.use(bodyparser.urlencoded({extended : true }));
     
     app.use(compression());
     app.use(cors());
-    app.use(bodyParser.json());
     app.use(express.static('public'));
 }
