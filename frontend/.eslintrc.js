afca73f9f7fe216c403d4b59feccd24f69390945
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2017-12-28 10:13:42 -0200

    Fixing issue: Não reconhecer váriavel global window e $
    Depois de configurado o ESLint, o único problema é que a variável global window e $ do JQuery, por só existirem no contexto do navegador.
    Uma rápida googada eu encontrei a seguinte resposta no StackOverflow: (https://stackoverflow.com/questions/39510736/eslint-dollar-is-not-defined-no-undef)

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
+   env: {
+    "browser": true,
+    "commonjs": true,
+    "es6": true,
+    "jquery": true},
     extends: ['airbnb-base'],
     rules: {
       semi: [2, 'never'],
