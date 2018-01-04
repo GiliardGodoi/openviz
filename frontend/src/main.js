88a10299158ce5cc97f11ce811964f7cc9ad5aa8
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2018-01-04 01:02:32 -0200

    DEFINIÇÃO DAS FUNÇÕES BÁSICAS DE CONTROLE DO FORM
    Definição das operações básicas das funções que farão controle do Formulário. Algumas operações básicas já foram definidas. Renomeação do atributo name de alguns inputs será necessária.
    
    As funções estão organizadas segundo o prefixo dos nomes dos métodos. Desta forma temos:
     - submit: gupo de funções responsáveis por realizar requisições ao servidor de dados.
     - event: funções que disparam algum evento
     - enable: habilita componente no formulário.
    
    Por exemplo:
     - *eventClickBtnSearch* significa: evento de clique no botão de pesquisa (event - Click - Btn - Search)
     - *eventChangeInputVlLicitacao* significa: evento quando da mudança do input VlLicitacao.
    
     Espero que eu mantenha o código do formulario bem organizado.
    
    This is it!

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,29 +5,23 @@
 // import ClusterForce from './chart/clusterforce'
 // import Barchart from './chart/barchartmultiple'
 
-const drawTable = function drawTable () {
+const compareStringValuesAsDate = function compareStringValuesAsDate (dtOne, dtTwo) {
+  return false
+}
+
+const submit = function submit (params) {
 
 }
 
-const drawChart = function drawChart () {
-  const nome = 'grafico de barras'
-  return nome
+const submitForm = function submitForm (params) {
+
 }
 
-const btnCleanAction = function btnCleanAction () {
-  $('select.form-control').val('invalid')
-  $('input.form-control').val(null)
+const submitInputNroAno = function submitInputNroAno (params) {
+
 }
 
-const btnSearchAction = function btnSearchAction () {
-  const params = $.param($('.form-control'))
-  console.log(params)
-}
-
-const inputMaskToMoney = function inputMaskToMoney (event) {
-}
-
-const autocompleteActive = function autocompleteActive () {
+const actionAutocomplete = function actionAutocomplete (params) {
   const source = [
     { label: 'SANTO ANTÔNIO DA PLATINA', id: '412410' },
     { label: 'JACAREZINHO', id: '411180' },
@@ -42,33 +36,71 @@ const autocompleteActive = function autocompleteActive () {
   })
 }
 
-const inputMaskToMoneyActive = function inputMaskToMoneyActive () {
-  $('#_inputValorLicitacaoMin, #_inputValorLicitacaoMax').keydown(inputMaskToMoney)
+const eventClickBtnSearch = function eventClickBtnSearch () {
+  let values = $('.form-control').serializeArray() || []
+  values = values.reduce((previous, current, index) => {
+    const key = current.name ? current.name : index
+    const newObj = { ...previous }
+    if (current.value) {
+      newObj[key] = current.value
+    }
+    return newObj
+  }, {})
+  return values
 }
 
-const datepickerActive = function datepickerActive () {
+const eventClickBtnClean = function eventClickBtnClean () {
+  $('input.form-control').val(null)
+  $('select.form-control').val('blank')
+  $('.form-control').not('select#_inputAno').attr('readonly', '')
+  $('select#_inputDescricaoModalidade.form-control').attr('disabled', '')
+  $('#_inputDataEditalMin, #_inputDataEditalMax, #_inputDataAberturaMin, #_inputDataAberturaMax').datepicker('destroy')
+}
+
+const eventChangeInputVlLicitacao = function eventChangeInputVlLicitacao (event) {
+
+}
+
+const eventChangeInputsDate = function eventChangeInputsDate (event) {
+
+}
+
+const eventActionDrawTable = function eventActionDrawTable (event) {
+
+}
+
+const eventActionDrawChart = function eventActionDrawChart (event) {
+
+}
+
+const enableDatapicker = function datepickerActive (ANO = '2013') {
   $('#_inputDataEditalMin, #_inputDataEditalMax, #_inputDataAberturaMin, #_inputDataAberturaMax')
     .datepicker({
       showOtherMonths: true,
       selectOtherMonths: true,
-      showOtherYears: true,
-      selectOtherYears: true,
+      showOtherYears: false,
+      selectOtherYears: false,
       changeMonth: true,
       changeYear: true,
-      minDate: new Date(2013, 0, 1),
-      maxDate: new Date(),
+      minDate: new Date(ANO, 0, 1),
+      maxDate: new Date(ANO, 11, 31),
+      defaultDate: new Date(ANO, 0, 1),
+      monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+      monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Maio', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
       dateFormat: 'dd/mm/yy',
     })
+}
+
+const enableForm = function enableForm (params) {
+  $('.form-control').attr('readonly', null).attr('disabled', null)
+  enableDatapicker()
+
+  $('#_btnSearch').click(eventClickBtnSearch)
+  $('#_btnClean').click(eventClickBtnClean)
 }
 
 window.onload = function onload () {
   $('#fullpage').fullpage({
     scrollBar: true,
   })
-  // $(datepickerActive)
-  // $(autocompleteActive)
-  // $(inputMaskToMoneyActive)
-
-  $('#_btnClean').click(btnCleanAction)
-  // $('#_btnSearch').click(btnSearchAction)
 }
