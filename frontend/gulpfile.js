0d4c00afe5c30cc3e780258d9e6ae48b187bad47
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2018-02-08 00:02:41 -0200

    CORRIGINDO FORMATAÇÃO ESLINT

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,35 +1,35 @@
-const gulp = require("gulp")
-const server = require("gulp-express")
-const browserify = require("browserify")
-const babelify = require("babelify")
+const gulp = require('gulp')
+const server = require('gulp-express')
+const browserify = require('browserify')
+const babelify = require('babelify')
 // const sourcemaps = require("gulp-sourcemaps")
-const source = require("vinyl-source-stream")
-const buffer = require("vinyl-buffer")
+const source = require('vinyl-source-stream')
+const buffer = require('vinyl-buffer')
 
-gulp.task("server", function(){
-    server.run(["server.js"]);
+gulp.task('server', () => {
+  server.run(['server.js'])
 
-    gulp.watch(["./src/**/*.html"], server.notify);
-    gulp.watch(["./src/**/*.js"],["code-js"]);
-});
+  gulp.watch(['./src/**/*.html'], server.notify)
+  gulp.watch(['./src/**/*.js'],['code-js'])
+})
 
-gulp.task("code-js", function(){
-    browserify({
-        entries : "src/main.js",
-        extensions : [".js"],
-        debug : true
-    })
-    .transform( babelify,{
-        presets : ["es2015", "stage-3"]
+gulp.task('code-js', () => {
+  browserify({
+    entries: 'src/main.js',
+    extensions: ['.js'],
+    debug: true,
+  })
+    .transform(babelify, {
+      presets: ['es2015', 'stage-3']
     })
     .bundle()
-    .on("error", (err) => {
-        console.log("ERRO ERRO ERROO");
-        console.log(err.toString());
+    .on('error', (err) => {
+      console.log('ERRO ERRO ERROO')
+      console.log(err.toString())
     })
-    .pipe(source("app.js"))
+    .pipe(source('app.js'))
     .pipe(buffer())
-    .pipe(gulp.dest("public/js"));
-});
+    .pipe(gulp.dest('public/js'))
+})
 
-gulp.task("default", ["server", "code-js"]);
+gulp.task('default', ['server', 'code-js'])
