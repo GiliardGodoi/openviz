1df9b5f60d0d0f56aadf9c7d7f14e79f140d8773
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2018-03-06 10:57:41 -0300

    ADEQUANDO A REGRAS DE FORMATAÇÃO DO ESLINT

--- frontend/server.js
+++ frontend/server.js
@@ -1,10 +1,13 @@
-const express = require("express");
-const app = module.exports.app = exports.app = express();
+const express = require('express')
+
+const app = express()
+module.exports.app = app
+exports.app = app
 
 // app.use(require("connect-livereload"));
-app.use(express.static("public"));
+app.use(express.static('public'))
 
 
 app.listen(8000, () => {
-    console.log("\t\t::: Frontend running at port 8000 :::");
+  console.log('\t\t::: Frontend running at port 8000 :::')
 })
