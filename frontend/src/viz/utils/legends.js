c3efc5263aa8f33908f1060166cd25834041a60a
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2018-03-09 09:49:09 -0300

    REFATORANDO A FORMA PARA CONSTRUIR/DESENHAR UMA LEGENDA

--- frontend/src/viz/utils/legends.js
+++ frontend/src/viz/utils/legends.js
@@ -26,18 +26,28 @@ function ordinalLegend (params) {
       .range(['rgb(153, 107, 195)', 'rgb(56, 106, 197)', 'rgb(93, 199, 76)', 'rgb(223, 199, 31)', 'rgb(234, 118, 47)'])
   }
 
-  const symbol = params.symbol || 'square'
   const size = params.size || 150
   const shapePadding = params.shapePadding || 5
   const scale = params.scale || ordinalScaleBuild()
 
-  const d3Symbol = switchSymbol(symbol)
+  const symbol = switchSymbol(params.symbol)
 
   const legend = d3.legendColor()
-    .shape('path', d3.symbol().type(d3Symbol).size(size)())
+    .shape('path', d3.symbol().type(symbol).size(size)())
     .shapePadding(shapePadding)
     .scale(scale)
   return legend
 }
 
+function drawLegend (params) {
+  const { container, legend } = params
+  const position = params.position || [0, 0]
+  const element = container.append('g')
+    .attr('class', 'legend-group legend')
+    .attr('transform', `translate(${position})`)
+    .call(legend)
+  return element
+}
+
 module.exports.ordinalLegend = ordinalLegend
+module.exports.drawLegend = drawLegend
